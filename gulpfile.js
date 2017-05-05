388e4bd94312649853f06377e683507b91f9ef94
eionut <jercan.ionut@gmail.com>
2017-05-05 18:22:35 +0300

    merge stream added

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
+var browserify = require('gulp-browserify');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 var SOURCEPATH = {
     sassSource: 'src/scss/*.scss',
@@ -28,15 +30,22 @@ gulp.task('clean-scripts', function() {
 });
 
 gulp.task('sass', function() {
-    return gulp.src(SOURCEPATH.sassSource)
+    var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
+    var sassFiles;
+
+    sassFiles = gulp.src(SOURCEPATH.sassSource)
         .pipe(autoprefixer())
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError));
+
+    return merge(bootstrapCSS, sassFiles)
+        .pipe(concat('app.css'))
         .pipe(gulp.dest(APPPATH.css));
 });
 
 gulp.task('scripts', ['clean-scripts'], function() {
    gulp.src(SOURCEPATH.jsSource)
        .pipe(concat('main.js'))
+       .pipe(browserify())
        .pipe(gulp.dest(APPPATH.js));
 });
 
